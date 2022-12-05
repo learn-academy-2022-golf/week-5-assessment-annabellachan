57bfde3bc9d58101d18e8efa17ada0b4f1543a89
annabellachan <annabellac24@gmail.com>
2022-12-04 22:01:01 -0800

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,6 +10,13 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+
+// describe("greeter", () => {
+//     it("returns a generic greeting", () => {
+//       expect(greeter()).toEqual("Hello, LEARN student!")
+//     })
+//   })
+
 // --------------------1) Create a function that takes in a string and returns a string with a coded message. The coded message converts "a" to 4, "e" to 3, "i" to 1, and "o" to 0.
 
 // a) Create a test with expect statements using the variables provided.
@@ -21,9 +28,52 @@ const secretCodeWord2 = "Gobbledygook"
 const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
+describe("codeConverter", (string) => {
+    it("takes in a string and returns a string with a coded message (a>4, e>3, i>1, o>0", () => {
+      expect(codeConverter(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(codeConverter(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(codeConverter(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+//  ReferenceError: codeConverter is not defined
+
 // b) Create the function that makes the test pass.
+    // Create a function codeConverter that takes in a string as the parameter
+    // Create a new array to convert a string to an array that holds new values 
+        // use .split to split the array
+        // use .map to iterate over the array
+    // use conditional to update all vowels lower and upper case to desired number
+        // a / A = 4
+        // e / E = 3
+        // i / I = 1
+        // o / O = 0
+    // return the array 
+    // return the array back to a string .join and coded messages with voels converted
+
+const codeConverter = (string) => {
+    let newArray = string.split('').map((word) => {
+        if(word === 'a' || word === 'A' ){
+            return '4'
+        } else if (word === 'e' || word === 'E' ) {
+            return '3'
+        } else if (word === 'i' || word === 'I' ) {
+            return '1'
+        } else if (word === 'o' || word === 'O' ) {
+            return '0'
+        } else {
+            return word
+        }
+    })
+    return newArray.join('')
+}
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
+
+
 
 // a) Create a test with expects statement using the variable provided.
 
@@ -34,7 +84,29 @@ const letterA = "a"
 const letterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
+describe("narrow", () => {
+    it(" takes in an array of words and a single letter and returns an array of all the words containing that particular letter", () => {
+      expect(narrow(fruitArray, letterA)).toEqual(["Mango", "Apricot", "Peach"])
+      expect(narrow(fruitArray, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
+//  ReferenceError: narrow is not defined
+
 // b) Create the function that makes the test pass.
+    // Create a function narrow that takes in 2 parameters: array and string
+        // use .filter to filter the array since looking for a shorter array
+        // use higher order function that takes the value from the array
+        // use .toLowercase to search the array
+            // this changes the array to all lower case so it can search if the array has a or e easier
+                // did not think of using .toLowerCase for a really long time and was stuck for a very long time......
+        // use .includes to check if the array has a or e in the string
+
+const narrow = (array,string) => {
+    return array.filter(value => value.toLowerCase().includes(string))
+   }
+
+// Test Suites: 1 passed, 1 total
+// Tests:       2 passed, 2 total
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
@@ -49,4 +121,37 @@ const hand3 = [5, 5, 5, 5, 4]
 const hand4 = [7, 2, 7, 2, 7]
 // Expected output: true
 
+describe("fullHouseChecker", () => {
+    it("takes in an array of 5 numbers and determines whether or not the array is a full house. A full house is exactly one pair and one three of a kind", () => {
+      expect(fullHouseChecker(hand1)).toEqual(true)
+      expect(fullHouseChecker(hand2)).toEqual(false)
+      expect(fullHouseChecker(hand3)).toEqual(false)
+      expect(fullHouseChecker(hand4)).toEqual(true)
+    })
+  })
+//  ReferenceError: fullHouseChecker is not defined
+
 // b) Create the function that makes the test pass.
+    // create a function called fullHouseChecker that takes in an array as the parameter
+    // use .sort to sort the array in ascending order
+    // Use a for loop to check if the first 3 numbers equal and last 2 are equal
+        // 
+    // If conditions do not meet, return false 
+
+
+const fullHouseChecker = (array) => {
+    let sort = array.sort()
+    for (let i = 0;i < array.length; i++){
+        if(sort[0] === sort[1] && sort[1] === sort[2] && sort[3] === sort[4]){
+            return true
+        } else if(sort[0] === sort[1] && sort[2] === sort[3] && sort[2] === sort[4]){
+            return true
+        } else {
+            return false
+        }
+            
+    }
+}
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,44 +8,45 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What does calling super do in a class?
 
-Your answer:
+Your answer: Ruby super is used to implement super class implementation of the current method. It can be used in subclasses and after being invoked, it has access to instant variables in that method.
 
-Researched answer:
+Researched answer: Ruby uses the super keyword to call the superclass implementation of the current method. Within the body of a method, calls to super acts just like a call to that original method. The search for a method body starts in the superclass of the object that was found to contain the original method.
 
 2. What is a gem?
 
-Your answer:
+Your answer: Ruby gems are  open source libraries that contain Ruby code and are packaged with a little extra data . Using a gem allows a programmer to use the code within the gem in their own program.
 
-Researched answer:
+Researched answer: Gem is a package manager that is designed to install libraries. With gems, it provides easy code sharing by allowing any dev to download gems that are necessary to any device allowing developers to publish and distribute. It can contain files, snippets of code, etc. 
 
 3. What is a relational database? Are there other kinds of databases?
 
-Your answer:
+Your answer: Relational databases are similar to excel sheets and contain information that are displayed in rows and columns. A database can have many tables that have relationships to each other. 
 
-Researched answer:
+Researched answer: Relational databases stores data in tables that can be related to one another through the use of primary keys. Each row in the table is a record with a unique ID called the key. Other kinds of databases like non-relational databases does not include rows and colomns like most traditional database systems. Non-relational databases use a storage model that specificall requires the type of data being stored.
 
 4. What are primary keys? Why are they important?
 
-Your answer:
+Your answer: Primary key are unique identifiers that makes it easier to differentiate between values and information that are stored.
 
-Researched answer:
+Researched answer: Primary keys are unique identifiers that are useful for differentiating between values that are stored. With primary keys, there can not be duplicates because it  prevents any overlapping of information and can be easily referred to when needed. It can be used to identify, organize, and create relationships between different databases. 
 
 5. What are the HTTP verbs? What is each verb's corresponding CRUD action?
 
-Your answer:
+Your answer: The most commonly used HTTP verbs are: POST (create), GET (read), PUT(update), PATCH(update or modify), and DELETE(delete.)
 
-Researched answer:
+Researched answer: 
+The HTTP verbs comprise a major portion of our “uniform interface” constraint and provide us the action counterpart to the noun-based resource. The primary or most-commonly-used HTTP verbs are POST, GET, PUT, PATCH, and DELETE. These correspond to CRUD operations. 
 
 ## Looking Ahead: Terms for Next Week
 
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: RESTful routing is a set of standards used in many different languages to create efficient and reusable routes. It aims to map HTTP methods and CRUD actiion together. This makes it easier for other developers to understand and navigate an application and results in clean, consistent URL paths for users.
 
-2. Ruby on Rails:
+2. Ruby on Rails: Web application framework used to build web applications. It is a gem created by Ruby that makes creating applications effortless since it provides a prebuilt structure. The model view controlloer is designed to provide structures for databases and pages.
 
-3. Database migrations:
+3. Database migrations: Database migration is the process of migrating data from one or more source databases to one or more target databases by using a database migration service. 
 
-4. Params:
+4. Params: Params is short for parameters. It can be used to pass values to a controller.
 
-5. ERB:
+5. ERB: ERB is embedded Ruby. Templates that allow codes to be added to a plain text document in Ruby. 
